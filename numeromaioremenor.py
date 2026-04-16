0155ecac026ae7acf9fcffd81d32f80a6642ab47
MrSpacePug <pugespaciall@gmail.com>
2026-04-16 20:16:22 -0300

    Add files via upload

---
+++ numeromaioremenor.py
@@ -0,0 +1,17 @@
+n1 = int(input('numero 1: '))
+n2 = int(input('numero 2: '))
+n3 = int(input('numero 3: '))
+
+if n1 > n2 and n1 > n3:
+    print('Numero 1 é o maior')
+elif n2 > n1 and n2 > n3:
+    print('Numero 2 é o maior')
+elif n3 > n1 and n3 > n2:
+    print('Numero 3 é o maior')
+
+if n1 < n2 and n1 < n3:
+    print('Numero 1 é o menor')
+elif n2 < n1 and n2 < n3:
+    print('Numero 2 é o menor')
+elif n3 < n1 and n3 < n2:
+    print('Numero 3 é o menor')
